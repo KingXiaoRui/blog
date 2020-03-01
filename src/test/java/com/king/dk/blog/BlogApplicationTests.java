0371e1b0c8bef1156c2cf0d4ac94b9c1165cce89
Mr.King <2484228578@qq.com>
2020-03-01 17:14:46 +0800

    first commit

---
+++ src/test/java/com/king/dk/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.dk.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
